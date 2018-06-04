7dad0ff5a6aa19b5ee08613c351b73fdf203c343
ryugibo <ryugibo@daum.net>
2018-06-04 16:52:34 +0900

    Fix Tabify, Build unigore

--- Source/Pinball.Target.cs
+++ Source/Pinball.Target.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 
 public class PinballTarget : TargetRules
 {
-    public PinballTarget(TargetInfo Target)
-        : base(Target)
+	public PinballTarget(TargetInfo Target)
+		: base(Target)
 	{
 		Type = TargetType.Game;
-        ExtraModuleNames.Add("Pinball");
-    }
+		ExtraModuleNames.Add("Pinball");
+	}
 }
